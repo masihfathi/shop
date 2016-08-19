30618c0c1b01304194b126149aea8624190f317b
masih <masih@masih-K42Jc>
2016-08-20 00:14:12 +0430

    controller added

---
+++ models/Categories.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "categories".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $confirmed
+ *
+ * @property Products[] $products
+ */
+class Categories extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'categories';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'confirmed'], 'required'],
+            [['confirmed'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'confirmed' => 'Confirmed',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProducts()
+    {
+        return $this->hasMany(Products::className(), ['category_id' => 'id']);
+    }
+}
